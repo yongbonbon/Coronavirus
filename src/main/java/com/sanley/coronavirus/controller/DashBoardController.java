e34fd20575a670dddb5b96edc02cb4176a7720f8
bonbon <595584479@qq.com>
2021-04-18 16:26:12 +0800

    map

---
+++ src/main/java/com/sanley/coronavirus/controller/DashBoardController.java
@@ -0,0 +1,37 @@
+package com.sanley.coronavirus.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * @Author: bonbon
+ * @Date: 2021/4/17 23:24
+ */
+@Controller
+public class DashBoardController {
+
+    @RequestMapping(value = "/dashboard/index")
+    @ResponseBody
+    public String toDashBoard(Model model) throws IOException {
+        URL u1 = new URL("https://zaixianke.com/yq/all");
+        URLConnection conn = u1.openConnection();
+        InputStream is = conn.getInputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(is,"UTF-8"));
+        String text1 = br.readLine();
+        /*model.addAttribute("text1", text1);
+        System.out.println(text1);*/
+        br.close();
+        return text1;
+    }
+
+    @RequestMapping(value = "/to/dashboard")
+    public String toDashBoards() {
+        return "dashboard";
+    }
+}
